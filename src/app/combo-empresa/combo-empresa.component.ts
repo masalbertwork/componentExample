1b93628e807f4ea23afe739fbb36594db064be46
Albert Mas <masalbert@gmail.com>
2019-10-16 16:52:13 +0200

    neteja codi empresa

--- src/app/combo-empresa/combo-empresa.component.ts
+++ src/app/combo-empresa/combo-empresa.component.ts
@@ -3,46 +3,23 @@ import {
   OnInit,
   Output,
   EventEmitter,
-  forwardRef,
-  ViewChild,
   Optional,
   Self,
-  AfterViewInit,
-  ChangeDetectorRef,
-  ChangeDetectionStrategy
+  ChangeDetectorRef
 } from '@angular/core';
 import {
   ControlValueAccessor,
   FormGroup,
   FormControl,
-  NG_VALUE_ACCESSOR,
-  NgControl,
-  AbstractControl,
-  ValidationErrors,
-  NG_VALIDATORS,
-  Validator
+  NgControl
 } from '@angular/forms';
 
 @Component({
   selector: 'app-combo-empresa',
   templateUrl: './combo-empresa.component.html',
   styleUrls: ['./combo-empresa.component.scss']
-  // changeDetection: ChangeDetectionStrategy.OnPush
-  // providers: [
-  //   {
-  //     provide: NG_VALUE_ACCESSOR,
-  //     useExisting: forwardRef(() => ComboEmpresaComponent),
-  //     multi: true
-  //   },
-  //   {
-  //     provide: NG_VALIDATORS,
-  //     useExisting: forwardRef(() => ComboEmpresaComponent),
-  //     multi: true
-  //   }
-  // ]
 })
-export class ComboEmpresaComponent
-  implements OnInit, ControlValueAccessor, AfterViewInit {
+export class ComboEmpresaComponent implements OnInit, ControlValueAccessor {
   @Output() selectElement: EventEmitter<any>;
   @Output() noSelectElement: EventEmitter<any>;
   myForm: FormGroup;
@@ -50,17 +27,14 @@ export class ComboEmpresaComponent
   selectedOption: any;
   noResult: boolean;
   isDisabled: boolean;
-  // @ViewChild('combo', { static: false, read: NgControl }) combo;
 
   onChange = (_: any) => {};
   onTouch = () => {};
-  // control;
 
   constructor(
     private cd: ChangeDetectorRef,
     @Optional() @Self() public ngControl: NgControl
   ) {
-    // private cdr: ChangeDetectorRef
     this.myForm = new FormGroup({
       user: new FormControl('')
     });
@@ -76,30 +50,19 @@ export class ComboEmpresaComponent
 
   ngOnInit() {}
 
-  ngAfterViewInit() {
-    // this.validate(null);
-  }
-
   onSelect(event) {
     this.onChange(event.value);
     this.selectElement.emit({ event });
-    // this.validate(this.myForm.get('user'));
   }
 
-  noSelect(event) {
-    // this.onChange(null);
-    // this.writeValue(null);
-    // this.noSelectElement.emit({ event });
-  }
+  noSelect(event) {}
 
   delete(event) {
-    // this.myForm.reset();
     this.onChange(null);
     this.writeValue(null);
   }
 
   tocat(event) {
-    console.log('tocat' + event);
     this.onTouch();
   }
 
@@ -119,20 +82,7 @@ export class ComboEmpresaComponent
 
   setDisabledState(isDisabled: boolean): void {}
 
-  // validate(control: AbstractControl): ValidationErrors | null {
-  //   const errors = this.myForm.get('user').errors;
-  //   if (errors) {
-  //     return { invalid: true };
-  //   } else {
-  //     this.myForm.setErrors(null);
-  //   }
-  //   return null;
-  // }
-
   refrescValidacio() {
-    console.log('HI HA ERROR!!');
     this.ngControl.control.setErrors({ invalid: true });
-    // this.myForm.reset();
-    // this.cdr.markForCheck();
   }
 }
